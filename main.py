fa62945c59e499f09c766a10970ae8d98f40c237
kobanya <nagy.bela.budapest@gmail.com>
2023-01-01 15:12:41 +0100

    az ablak  méretének konfigurálása

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# az ablak méretének beállítása
+from kivy.config import  Config
+Config.set("graphics","width", "1280")
+Config.set("graphics","height", "720")
+# az ablak átméretezhetőségének tiltása=0, engedélyezése=1
+Config.set("graphics","resizable", "0")
 
 from kivy.app import App
 from kivy.uix.widget import Widget
